1cffc0709631118c4812ebca32a1cb2a8afa31cb
Vinícius Gonzaga Pereira <166918377+viniciugonzaga@users.noreply.github.com>
2024-11-01 15:41:40 -0300

    01/11/24 atividade calculadora

---
+++ Estudos/calc/scriptcalc.js
@@ -0,0 +1,23 @@
+// script.js
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+function deleteLast() {
+    let display = document.getElementById("display");
+    display.value = display.value.slice(0, -1);
+}
+
+function appendValue(value) {
+    document.getElementById("display").value += value;
+}
+
+function calculateResult() {
+    let display = document.getElementById("display");
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "Error";
+    }
+}
