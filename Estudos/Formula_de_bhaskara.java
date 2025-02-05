8a51297007e32918bec84a481ddfc225647b8467
Vinícius Gonzaga Pereira <166918377+viniciugonzaga@users.noreply.github.com>
2025-02-05 16:32:22 -0300

    05/02

---
+++ Estudos/Formula_de_bhaskara.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.formula_de_bhaskara;
+
+import java.util.Scanner;
+
+
+
+public class Formula_de_bhaskara {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+       System.out.print("Digite o valor do termo A: ");
+        int a = scanner.nextInt();
+        
+      System.out.print("Digite o valor do termo B: ");
+        int b = scanner.nextInt();
+        
+       System.out.print("Digite o valor do termo C: ");
+        int c = scanner.nextInt(); 
+        
+        double delta = (b * b)+ (-4 *(a * c));
+        
+      
+        
+        
+        
+        
+        
+        
+        
+    }
+}
