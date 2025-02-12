2956ae2d8c88e275cd44b81ce142fe7d2841ef69
Vinícius Gonzaga Pereira <166918377+viniciugonzaga@users.noreply.github.com>
2025-02-12 16:31:23 -0300

    12/02

---
+++ carlos/exemplocarro/Carro.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.exemplocarro;
+
+/**
+ *
+ * @author dti
+ */
+public class Carro {
+    String cor;
+    String modelo;
+    String motor;
+    String nome;
+    
+    
+    void ligar(){
+        System.out.println("Ligando o carro");
+    }
+    void desligar(){
+         System.out.println("Desligando o carro");
+    }
+     void acelerar(){
+         System.out.println("Acelerando o carro");
+    }
+      void brecar(){
+         System.out.println("Brecando o carro");
+    }
+       void mudarMarcha(){
+         System.out.println("Engatando a marcha");
+    }
+    
+}
