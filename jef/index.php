9da2c345f69bb7e66b6164f5d6edc18d812ef080
Vinícius Gonzaga Pereira <166918377+viniciugonzaga@users.noreply.github.com>
2025-02-13 16:33:10 -0300

    13

---
+++ jef/index.php
@@ -0,0 +1,33 @@
+<html lang="pt-br">
+  <head>
+    <title>Título da página</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+
+ <?php
+
+echo "Caluclo de Numeros <Br>";
+
+/*Soma*/
+ $s = 20;
+ $m = 20;
+
+ $soma = $s + $m;
+ $menos = $s - $m;
+ $divisão = $s / $m;
+ $multi = $s . $m;
+
+echo $soma;
+echo " soma<br>";
+echo $menos;
+echo " menos<br>";
+echo $divisão;
+echo " divisão<br>";
+echo $multi;
+echo " multiplicação<br>";
+ ?>
+
+
+  </body>
+</html>
