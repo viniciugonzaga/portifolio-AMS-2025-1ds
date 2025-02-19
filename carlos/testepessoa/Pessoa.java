d7fccb5dc578f9f3d13b176bdb69063b56a941fb
Vinícius Gonzaga Pereira <166918377+viniciugonzaga@users.noreply.github.com>
2025-02-19 16:38:36 -0300

    testePessoa

---
+++ carlos/testepessoa/Pessoa.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.testepessoa;
+
+/**
+ *
+ * @author dti
+ */
+public class Pessoa {
+    
+    String nome;
+    String idade;
+    
+    void definirNome(String Valor){
+        nome = ("João");
+    }
+      String retornarNome (){
+        return nome;
+    }
+      
+    void definirIdade(String Valor){
+        idade = ("16");
+    }
+     String retornarIdade (){
+        return idade;
+    }
+    
+} 
---
+++ carlos/testepessoa/TestePessoa.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testepessoa;
+
+/**
+ *
+ * @author dti
+ */
+public class TestePessoa {
+
+    public static void main(String[] args) {
+        
+        Pessoa pessoa = new Pessoa();
+        
+        
+        
+        aluno aluno = new aluno();
+        
+        
+      
+        
+        
+    }
+}
