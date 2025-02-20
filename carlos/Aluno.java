3f362af0516a024181ca62b28a5a7a409dbe6cc5
Vinícius Gonzaga Pereira <166918377+viniciugonzaga@users.noreply.github.com>
2025-02-20 13:52:28 -0300

    Add files via upload

---
+++ carlos/Aluno.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.testepessoa;
+
+/**
+ *
+ * @author dti
+ */
+public class Aluno extends Pessoa{
+    
+    String curso;
+   
+    void Aluno(){
+        this.curso = curso;
+    }
+   
+    
+    void definirCurso(String val_curso){
+      
+        curso = val_curso;
+    }
+    void retornarCurso(){
+        
+         System.out.println(curso);
+    }
+}
