1b6d623242822669c66081b63cd71231c3b467ad
Vinícius Gonzaga Pereira <166918377+viniciugonzaga@users.noreply.github.com>
2025-02-20 16:30:25 -0300

    20/02

--- jef/index.php
+++ jef/index.php
@@ -1,33 +1,70 @@
-<html lang="pt-br">
-  <head>
-    <title>Título da página</title>
-    <meta charset="utf-8">
-  </head>
-  <body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>aula20/02</title>
+</head>
+<body>
+    
 
- <?php
+<?php
 
-echo "Caluclo de Numeros <Br>";
+$x = 10;
+$y = 20;
+$z = 2;
 
-/*Soma*/
- $s = 20;
- $m = 20;
+echo "<Br> 1)";
+if($z == 2){
+    $soma = $x + $y;
+    echo  "soma:$soma<br>";
+}elseif($z == 2){
 
- $soma = $s + $m;
- $menos = $s - $m;
- $divisão = $s / $m;
- $multi = $s . $m;
+    $menos = $x - $y;
+    echo "menos:$menos<br>";
+}elseif($z == 3){
 
-echo $soma;
-echo " soma<br>";
-echo $menos;
-echo " menos<br>";
-echo $divisão;
-echo " divisão<br>";
-echo $multi;
-echo " multiplicação<br>";
- ?>
+    $divisao = $x / $y;
+echo "divisão:$divisao<br>";
+}else{
+    $multi = $x * $y;
+    echo "multiplicação:$multi<br>";
+
+}
+
+$a = 1;
+echo "<Br> 2)";
+while($a <= 10){
+    echo "numeração: $a<br>";
+    $a = $a + 1;
+
+}
+echo "<Br> 3)";
+for($s = 1; $s <=20; $s = $s+1){
+echo " <Br>$s";
+
+}
+echo"<br>";
+echo "<Br> 4)";
+echo"<br>";
+$b = 0;
+do{
+echo $y;
+$b = $b+1;
+}while($b <= 10);
+
+echo "<br> 5)";
+
+$p = 1;
+$t= 10;
+$n= $p*$t;
+
+for($s = 1; $s <=20; $s = $s+1){
+    echo " <Br>$s";
+}
+
+?>
 
 
-  </body>
+</body>
 </html>
