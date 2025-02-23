7d8f9973a06c58b041762570f1dda0cb8c845f36
Vinícius Gonzaga Pereira <166918377+viniciugonzaga@users.noreply.github.com>
2025-02-23 03:03:29 -0300

    aula1

---
+++ jef/aula1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+    echo "Olá Mundo!";
+
+    for ($i = 1; $i <= 10; $i++){
+        echo "10 x $i = " . (10 * $i) . "<br>";
+    }
+    
+    ?>
+    
+</body>
+</html>
