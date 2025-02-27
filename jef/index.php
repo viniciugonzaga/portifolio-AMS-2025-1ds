8077d81f9efb350ff26ce67fb88cfcbaa4525777
Vinícius Gonzaga Pereira <166918377+viniciugonzaga@users.noreply.github.com>
2025-02-27 15:10:35 -0300

    aula1 correção

--- jef/index.php
+++ jef/index.php
@@ -1,70 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>aula20/02</title>
-</head>
-<body>
-    
-
 <?php
+      
+      for($i = 1; $i <= 2; $i = $i +1)
+     //1,2,3,4,5,6,7,8,9,10
 
-$x = 10;
-$y = 20;
-$z = 2;
+    for($x = 0; $x <= 10; $x = $x+1){
+        $resultado = $i * $x;
+        echo $i . "x" . $x . "=" . $resultado . "<br>";
 
-echo "<Br> 1)";
-if($z == 2){
-    $soma = $x + $y;
-    echo  "soma:$soma<br>";
-}elseif($z == 2){
-
-    $menos = $x - $y;
-    echo "menos:$menos<br>";
-}elseif($z == 3){
-
-    $divisao = $x / $y;
-echo "divisão:$divisao<br>";
-}else{
-    $multi = $x * $y;
-    echo "multiplicação:$multi<br>";
-
-}
-
-$a = 1;
-echo "<Br> 2)";
-while($a <= 10){
-    echo "numeração: $a<br>";
-    $a = $a + 1;
-
-}
-echo "<Br> 3)";
-for($s = 1; $s <=20; $s = $s+1){
-echo " <Br>$s";
-
-}
-echo"<br>";
-echo "<Br> 4)";
-echo"<br>";
-$b = 0;
-do{
-echo $y;
-$b = $b+1;
-}while($b <= 10);
-
-echo "<br> 5)";
-
-$p = 1;
-$t= 10;
-$n= $p*$t;
-
-for($s = 1; $s <=20; $s = $s+1){
-    echo " <Br>$s";
-}
-
+        echo "<br>";
+    }
+  
 ?>
-
-
-</body>
-</html>
