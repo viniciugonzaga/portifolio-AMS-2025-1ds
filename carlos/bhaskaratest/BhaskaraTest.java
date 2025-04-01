feaf49fcf4340f3f971836c5d88cead7abe3ca89
Vinícius Gonzaga Pereira <166918377+viniciugonzaga@users.noreply.github.com>
2025-04-01 10:26:11 -0300

    Add files via upload

---
+++ carlos/bhaskaratest/BhaskaraTest.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bhaskaratest;
+
+import javax.swing.*;  
+import java.awt.*;  
+import java.awt.event.*;  
+
+public class BhaskaraTest extends JFrame {
+    public static void main(String[] args) {
+        
+        BhaskaraExecult Bha = new BhaskaraExecult();
+        Bha.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Bha.setVisible(true);
+    }
+}
+       
