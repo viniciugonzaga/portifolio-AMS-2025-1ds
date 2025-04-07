bc666e64f4e261b54e707d7bb0c5f03cba049291
Vinícius Gonzaga Pereira <166918377+viniciugonzaga@users.noreply.github.com>
2025-04-07 18:08:45 -0300

    07

---
+++ marcelo/TextField/app/src/main/java/com/example/textfield/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.example.textfield.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+val darkblue = Color(0xff2F1EFA)
+val roxo = Color(0xff5B17C0)
+val preto = Color(0xff000000)
+val vermelho = Color(0xfE01F13)
+
